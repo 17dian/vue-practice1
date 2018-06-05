0744091ea9769dbf770821ca98330373cd098e8b
dongyang <843408503@qq.com>
2018-06-05 16:36:19 +0800

    add header swiper

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>wome</div>
+  <div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+  </div>
 </template>
 
 <script>
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper
+  }
 }
 </script>
 
